738353e923e76c312db97dceaaba00bad5988cf4
Kwangjoo <pkj5257@gmail.com>
2020-07-27 01:11:31 +0800

    final

--- client/src/components/module/ModuleSearch.js
+++ client/src/components/module/ModuleSearch.js
@@ -62,53 +62,58 @@ const ModuleSearch = ({
 
   const FoundModule = () => {
     return (
-      <Fragment>
-        <div class="ui fluid raised card">
-          <div class="content">
-            <div class="header">{module.title}</div>
-            <div class="meta">{module.moduleCode}</div>
-            <div class="description">
-              <p>{module.description}</p>
+      profile !== null && (
+        <Fragment>
+          <div class="ui fluid raised card">
+            <div class="content">
+              <div class="header">{module.title}</div>
+              <div class="meta">{module.moduleCode}</div>
+              <div class="description">
+                <p>{module.description}</p>
+              </div>
+            </div>
+            <div class="extra content">
+              <Link
+                to={`forums/${module.forum._id}`}
+                class="ui button basic primary"
+              >
+                Go to Forum
+              </Link>
+
+              {!(hasCompleted(module._id) || hasTaken(module._id)) &&
+              addModuleClick ? (
+                <button onClick={addModule} className="ui button basic primary">
+                  Add to modules
+                </button>
+              ) : (
+                <button class="ui disabled button">Already added</button>
+              )}
             </div>
           </div>
-          <div class="extra content">
-            <Link
-              to={`forums/${module.forum._id}`}
-              class="ui button basic primary"
-            >
-              Go to Forum
-            </Link>
+        </Fragment>
+      )
+    );
+  };
 
-            {!(hasCompleted(module._id) || hasTaken(module._id)) &&
-            addModuleClick ? (
-              <button onClick={addModule} className="ui button basic primary">
-                Add to modules
-              </button>
-            ) : (
-              <button class="ui disabled button">Already added</button>
-            )}
+  const NoProfile = () => {
+    return (
+      <Fragment>
+        <div class="ui warning message">
+          <i class="close icon"></i>
+          <div class="header">
+            You need to create a profile for this feature!
           </div>
+          Click on the link below to create your profile
         </div>
+        <Link to="/createprofile">Click here to make your profile</Link>
       </Fragment>
     );
   };
-  const ModuleNotFound = () => <div>Module is not found</div>;
 
   const { modCode } = modData;
-  return (
+  return profile !== null ? (
     <div style={{ marginTop: "15px" }}>
       <form className="ui fluid form" onSubmit={onSubmit}>
-        {/* <div class="ui fluid icon input">
-          <input
-            type="text"
-            name="modCode"
-            value={modCode}
-            onChange={onChange}
-            placeholder="Search for a module"
-          />
-          <div class="ui pointing label">Please enter a value</div>
-          <i type="submit" class="search icon"></i>
-        </div> */}
         <div class="field">
           <input
             type="text"
@@ -124,8 +129,10 @@ const ModuleSearch = ({
           )}
         </div>
       </form>
-      {!loading && profile !== null && module !== null && <FoundModule />}
+      {!loading && module !== null && <FoundModule />}
     </div>
+  ) : (
+    <NoProfile />
   );
 };
 
